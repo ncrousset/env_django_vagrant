64d1422973b211311cffe3ab8c292ac9c099f80b
Rudys Natanael Acosta Crousset <ncrousset@MacBook-Pro.home>
2020-06-13 13:43:36 -0400

    add python 3.7 and setting it as default in python3 alies

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+apt-get update && sudo apt-get upgrade
+
+apt-get install -y software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
 
 apt-get update
 
@@ -10,8 +15,10 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again p
 apt-get install -y nmon
 apt-get install -y nginx
 apt-get install -q -y -f mysql-server
+apt install -y python3.7
 
-
+update-alternatives --remove python3 /usr/bin/python3.4
+update-alternatives --install /usr/bin/python3  python3 /usr/bin/python3.7 1
 
 service mysql restart
 service nginx start
